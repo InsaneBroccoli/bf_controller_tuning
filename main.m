fba16cf7f6f387605c0b9e576010e96d419655f6
janick00 <janick.dort@bluewin.ch>
2025-10-28 13:28:48 +0100

    Refactor plotting logic into plot_utils class
    Introduced a new plot_utils helper class to centralize and standardize plotting functions. Updated main_class to expose relevant flight data for plotting, and refactored main.m to use plot_utils for all plotting, removing duplicated plotting code. This improves code organization, reusability, and maintainability.

--- main.m
+++ main.m
@@ -1,8 +1,9 @@
 clc, clear variables
 addpath(genpath('lib_michi'))
-addpath(genpath('lib')); % fügt lib/ und auch alle unterordner hinzu
+addpath(genpath('lib'));
 addpath logs/
 addpath utils/
+addpath class\
 %%
 % Choose an axis: 1: roll, 2: pitch, 3: yaw
 ind_ax = 1;
@@ -26,11 +27,17 @@ log_name2 = '20250908_flipmini_00.bbl.csv';
 
 file_path2 = fullfile(log_folder2, flight_folder2, log_name2);
 
-
 % Evaluation parameters
 do_compensate_iterm  = false;
 do_show_spec_figures = true;
 do_insert_legends    = true;
+
+
+pu = plot_utils;
+pu.second_flight = second_flight;
+pu.do_insert_legends = do_insert_legends;
+pu.linewidth = linewidth;
+
 
 % Defines
 set(cstprefs.tbxprefs, 'MagnitudeUnits', 'abs');
@@ -45,6 +52,7 @@ set(0, 'defaultAxesColorOrder', get_my_colors);
 % Bodeoptions
 opt = bodeoptions('cstprefs');
 
+pu.opt = opt;
 
 % Parameter first flight
 % type: 0: PT1, 1: BIQUAD, 2: PT2, 3: PT3
@@ -82,57 +90,17 @@ end
 flight1 = main_code.main_class(file_path1, para_new, ind_ax, do_compensate_iterm, ...
     do_show_spec_figures, do_insert_legends, opt, P_new, I_ratio_new, D_new);
 flight1 = flight1.run();
+
 if second_flight
     flight2 = main_code.main_class(file_path2, para_new, ind_ax, do_compensate_iterm, ...
         do_show_spec_figures, do_insert_legends, opt, P_new, I_ratio_new, D_new);
     flight2 = flight2.run();
-end
-
-%% Do plots
-
-figure(1)
-
-% --- Roll ---
-ax(1) = subplot(311); hold(ax(1),'on')
-plot(ax(1), flight1.gyroData.time, flight1.gyroData.roll)
-if do_insert_legends, legend('setpoint F1', 'gyro F1', 'gyroADC F1', 'location', 'best'), end
-if second_flight
-    plot(ax(1), flight2.gyroData.time, flight2.gyroData.roll)
-    if do_insert_legends, legend('setpoint F1', 'gyro F1', 'gyroADC F1', ...
-            'setpoint F2', 'gyro F2', 'gyroADC F2', 'location', 'best'), end
-end
-grid(ax(1),'on'); ylabel(ax(1),'Roll (deg/sec)')
-title(ax(1),'Gyro Signals')
-
-
-% --- Pitch ---
-ax(2) = subplot(312); hold(ax(2),'on')
-plot(ax(2), flight1.gyroData.time, flight1.gyroData.pitch)
-if second_flight
-    plot(ax(2), flight2.gyroData.time, flight2.gyroData.pitch)
-end
-legend(ax(2),'off')
-grid(ax(2),'on'); ylabel(ax(2),'Pitch (deg/sec)')
-
-
-% --- Yaw ---
-ax(3) = subplot(313); hold(ax(3),'on')
-plot(ax(3), flight1.gyroData.time, flight1.gyroData.yaw)
-if second_flight
-    plot(ax(3), flight2.gyroData.time, flight2.gyroData.yaw)
-end
-legend(ax(3),'off')
-grid(ax(3),'on'); ylabel(ax(3),'Yaw (deg/sec)'); xlabel(ax(3),'Time (sec)')
-
-% Achsen verlinken & Limits setzen
-linkaxes(ax,'x');
-
-if second_flight
-    xmax = max([flight1.gyroData.time; flight2.gyroData.time]);
+    pu.plotGyroSignals(flight1, flight2);
+    %pu.plotGyroSpectra(flight1, flight2); to be done!
+    %pu.plotOverview (flight1, flight2); to be done!
 else
-    xmax = max(flight1.gyroData.time);
+    pu.plotGyroSignals(flight1);
+    pu.plotGyroSpectra(flight1);
+    pu.plotOverview (flight1);
 end
-xlim(ax, [0 xmax]);
 
-% Styling
-set(findall(gcf,'type','line'), 'linewidth', linewidth)
