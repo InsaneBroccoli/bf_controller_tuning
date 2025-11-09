d52148b763c331827124ba006ba36af904c4af37
InsaneBroccoli <aware_lucite0l@icloud.com>
2025-11-09 13:06:47 +0100

    deleted unnessecary inclusion

--- best_practice/simple_csv_reader.py
+++ best_practice/simple_csv_reader.py
@@ -6,8 +6,6 @@ No external functions required (only pandas, numpy, matplotlib)
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-# from pathlib import Path
 import time as time_module
 import pickle
 
