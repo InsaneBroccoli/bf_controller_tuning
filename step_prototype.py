1196273becc1f8ce61a153a56f3993da8d8a2d1e
InsaneBroccoli <aware_lucite0l@icloud.com>
2025-11-09 17:00:41 +0100

    env removed defaults cleaned up workspace

--- step_prototype.py
+++ step_prototype.py
@@ -1,8 +1,3 @@
-"""
-Simple CSV Reader - Extract Setpoint and GyroUnfilt Data
-No external functions required (only pandas, numpy, matplotlib)
-"""
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
